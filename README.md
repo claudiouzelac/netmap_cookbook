404d1dc25e85d7a046637a98fd6c21e8044f32f3
Stewart Henderson <stewart.henderson@live.com>
2014-04-14 16:41:45 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ netmap_cookbook
 ===============
 
 Netmap Chef cookbook
+
+Description
+===========
+
+This cookbook builds netmap from source on the host box
+and installs it.
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name              "netmap"
+maintainer        "Stewart Henderson"
+maintainer_email  "stewart.henderson@live.com"
+license           "MIT"
+description       "Builds & installs netmap from source"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+depends           "git"
